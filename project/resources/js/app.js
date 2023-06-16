a9c921b52c46d26ee1111def2a592460ef0b174f
Ivan Izotov <it@myfxchoice.com>
2023-06-16 16:07:52 +0300

    добавлен powergrid

--- project/resources/js/app.js
+++ project/resources/js/app.js
@@ -1,7 +1,16 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import flatpickr from "flatpickr";
+import TomSelect from "tom-select";
 
 window.Alpine = Alpine;
+window.flatpickr = flatpickr;
+window.TomSelect = TomSelect;
+
+import './../../node_modules/flatpickr/dist/flatpickr.min.css';
+import './../../vendor/power-components/livewire-powergrid/dist/tom-select.css';
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid.css';
+
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
 
 Alpine.start();
--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -14,4 +14,12 @@
             </div>
         </div>
     </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <livewire:user-grid/>
+        </div>
+    </div>
+
+
 </x-app-layout>
